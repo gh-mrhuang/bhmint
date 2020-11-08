1be90592c676107a81c7c61b1a50c1d96932b1bc
[gh-mrhuang] <[454288016@qq.com]>
2020-11-08 17:40:16 +0800

    添加常用服务模块

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -40,15 +40,32 @@
         <div v-for="(item, index) in taskList.taskData" :key="index">
           <div @click="openUrl(item.formUrl)" class="home-task_item">
             <div>{{item.subject}}</div>
-            <div>{{item.createTime}}</div>
+            <div>{{getCreatedTime(item.createTime)}}</div>
           </div>
         </div>
       </van-collapse-item>
     </van-collapse>
 
-    <!-- <van-collapse v-model="activeCollapseAllways" class="home-task">
-      <van-collapse-item title="常用服务">内容</van-collapse-item>
-    </van-collapse> -->
+    <van-collapse v-model="activeCollapseAllways" class="home-task">
+      <van-collapse-item title="常用服务">
+        <div class="home-server_oa">
+          <div
+            v-for="item in cyServerList"
+            :key="item.WID"
+            @touchstart="startTouch(item.WID)"
+            @touchend="endTouch"
+            @click="openUrl(item.APPID, 1)"
+            class="home-server_oaitem">
+            <mt-image :src="item.PICSRC" width="40px" height="40px"></mt-image>
+            <div>{{item.APPNAME}}</div>
+          </div>
+          <div class="home-server_oaitem" @click="openPopup()">
+            <div class="home-server_add">+</div>
+            <div>添加服务</div>
+          </div>
+        </div>
+      </van-collapse-item>
+    </van-collapse>
 
     <div class="home-server">
       所有服务
@@ -64,7 +81,7 @@
           </div>
         </template>
         <div class="home-server_oa">
-          <div v-for="e in item.apps" :key="e.appId" class="home-server_oaitem">
+          <div v-for="e in item.apps" :key="e.appId" class="home-server_oaitem" @click="openUrl(e.openUrl)">
             <mt-image :src="e.iconUrl" width="40px" height="40px"></mt-image>
             <div>{{e.name}}</div>
           </div>
@@ -89,11 +106,24 @@
       </div>
     </mt-popup>
 
+    <mt-popup
+      v-model="serverPopupVisible"
+      position="bottom">
+      <div class="home-serve-List">
+        <div v-for="item in appAllList" :key="item.appId" class="home-server_oaitem" @click="clickServer(item)">
+          <mt-image :src="item.iconUrl" width="40px" height="40px"></mt-image>
+          <div>{{item.name}}</div>
+        </div>
+      </div>
+    </mt-popup>
+
   </div>
 </template>
 
 <script type="text/javascript">
 import logo from '@/assets/imgs/_logo.png'
+import { MessageBox } from 'bh-mint-ui2';
+import { Toast } from 'bh-mint-ui2';
 
 export default {
   data(){
@@ -111,7 +141,10 @@ export default {
         {title: '123', date: '2020/09/11'},
         {title: '123', date: '2020/09/11'},
         {title: '123', date: '2020/09/11'},
-      ]
+      ],
+      cyServerList: [],
+      touchTimeID: '',
+      serverPopupVisible: false
     }
   },
   watch: {
@@ -121,29 +154,58 @@ export default {
       })
     }
   },
+  computed: {
+    appAllList() {
+      const arr = []
+      if (Array.isArray(this.allServerList)) {
+        this.allServerList.forEach(item => {
+          if (!Array.isArray(item.apps)) return
+          arr.push(...item.apps)
+        })
+      }
+      return arr
+    }
+  },
   created(){
     // global.SDK.setTitleText("首页")
-
-    // 轮播图
-    this.$get('https://imy.sgmart.edu.cn/psfw/sys/smgxwfbxt/xyyw/getGGInfo.do').then(res => {
-      this.swipeList = res
-    })
-
-    // 所有应用
-    this.$get('https://imy.sgmart.edu.cn/newmobile/client/userAppListGroupByCategory').then(res => {
-      this.allServerList = res.data.datas || []
-    })
-
-    // 代办任务
-    this.$jsonp({
-      url: 'https://imy.sgmart.edu.cn/taskcenterapp/sys/taskCenter/portalDataReceive/getAllPortalTask.do',
-      // params: {_: 1604715457736},
-      success: (data) => {
-        this.taskList = data.queryTodoTask
-      }
-    })
+    this.getSwipeList()
+    this.getCyServerList()
+    this.getAllServerList()
+    this.getTaskList()
   },
   methods: {
+    getSwipeList(cb = () => {}) {
+      // 轮播图
+      this.$get('https://imy.sgmart.edu.cn/psfw/sys/smgxwfbxt/xyyw/getGGInfo.do').then(res => {
+        this.swipeList = Array.isArray(res.data) ? res.data : []
+        cb()
+      })
+    },
+    getCyServerList(cb = () => {}) {
+      // 常用服务
+      this.$get('https://imy.sgmart.edu.cn/psfw/sys/smgzdycdlxt/zdycdl/getCollectAppList.do').then(res => {
+        this.cyServerList = [{"WID":"a4c46220-4de6-48d9-a5ee-72790c285b88","PICSRC":"https://imy.sgmart.edu.cn/resources/app/5993136071349648/1.0_TR1/icon_72.png?_\u003d1599317644000","APPID":"5993136071349648","APPNAME":"苏美工自定义菜单栏系统","COLLECTOR":"ampadmin"},{"WID":"c0187c55-795a-4176-9282-64d76db31207","PICSRC":"https://imy.sgmart.edu.cn/resources/app/5941082618903015/1.0_TR1/icon_72.png?_\u003d1599708773000","APPID":"5941082618903015","APPNAME":"新闻发布系统","COLLECTOR":"ampadmin"},{"WID":"771dceba-08d4-46d6-86da-5ea2129174cc","PICSRC":"https://imy.sgmart.edu.cn/resources/app/6001591337172457/1.0_R1/icon_72.png?_\u003d1600234920000","APPID":"6001591337172457","APPNAME":"教务管理系统","COLLECTOR":"ampadmin"},{"WID":"4f8af069-96eb-4d35-9fc8-bca29e573f67","PICSRC":"https://imy.sgmart.edu.cn/resources/app/5995657239485927/1.0_EM1/icon_72.png?_\u003d1602859939000","APPID":"5995657239485927","APPNAME":"教职工请假申请单","COLLECTOR":"ampadmin"}]
+        cb()
+      })
+    },
+    getAllServerList(cb = () => {}) {
+      // 所有应用
+      this.$get('https://imy.sgmart.edu.cn/newmobile/client/userAppListGroupByCategory').then(res => {
+        this.allServerList = res.data.datas || []
+        cb()
+      })
+    },
+    getTaskList(cb = () => {}) {
+      // 代办任务
+      this.$jsonp({
+        url: 'https://imy.sgmart.edu.cn/taskcenterapp/sys/taskCenter/portalDataReceive/getAllPortalTask.do',
+        // params: {_: 1604715457736},
+        success: (data) => {
+          this.taskList = data.queryTodoTask
+          cb()
+        }
+      })
+    },
     handlerOpen() {
       this.popupVisible = true
     },
@@ -153,8 +215,69 @@ export default {
     handlePassword() {
 
     },
-    openUrl(data) {
+    openUrl(data, flag = 0) {
+      if (flag === 1) {
+        data = `https://imy.sgmart.edu.cn/appShow?appId=${data}`
+      }
       location.href = data
+    },
+    getCreatedTime(data) {
+      return data.split(' ')[0]
+    },
+    startTouch(id) {
+      clearTimeout(this.touchTimeID);
+      this.touchTimeID = setTimeout(() => {
+        this.openDeleteDialog(id)
+      }, 1000)
+    },
+    endTouch() {
+      clearTimeout(this.touchTimeID);
+    },
+    openDeleteDialog(id) {
+      MessageBox.confirm('确认删除此常用服务？', '提示').then(res => {
+        if (res !== 'confirm') return
+        this.$get(
+          'https://imy.sgmart.edu.cn/psfw/sys/smgzdycdlxt/zdycdl/DeleteCollectApp.do',
+          { WID: id }
+        ).then(res => {
+          this.getCyServerList(() => {
+            Toast({
+              message: '删除成功',
+              position: 'top',
+              duration: 1000
+            })
+          })
+          
+        }).catch(err => {
+          Toast({
+            message: '删除失败',
+            position: 'top',
+            duration: 1000
+          })
+        })
+      })
+    },
+    openPopup() {
+      this.serverPopupVisible = true
+    },
+    clickServer(data) {
+      this.$get(
+        'https://imy.sgmart.edu.cn/psfw/sys/smgzdycdlxt/zdycdl/AddCollectApp.do',
+        {
+          appId: data.appId,
+          appName: data.name,
+          picSrc: data.iconUrl,
+        }
+      ).then(res => {
+        this.getCyServerList(() => {
+          Toast({
+            message: '添加成功',
+            position: 'top',
+            duration: 1000
+          })
+          this.serverPopupVisible = false
+        })
+      })
     }
   }
 }
@@ -282,7 +405,7 @@ export default {
 
 <style>
 .page_border .v-modal {
-  background-color: transparent;
+  /* background-color: transparent; */
 }
 .page_border .mt-bColor-grey-lv5 {
   background-color: #fff;
@@ -309,6 +432,17 @@ export default {
   justify-content: center;
   align-items: center;
 }
+.home-server_oaitem .home-server_add {
+  width: 40px;
+  height: 40px;
+  background: #DFDFDF;
+  border-radius: 50%;
+  color: #fff;
+  font-weight: 700;
+  font-size: 30px;
+  line-height: 32px;
+  text-align: center;
+}
 .home-server_oaitem div {
   width: 75px;
   overflow: hidden;
@@ -320,5 +454,12 @@ export default {
 }
 .home-swipe .mint-swipe-indicators {
   bottom: 35px;
+}
+.home-serve-List {
+  height: 50vh;
+  overflow: auto;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
 }
 </style>
